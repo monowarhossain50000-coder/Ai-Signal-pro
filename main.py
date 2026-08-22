73e0e18ad3beb2d9bdbf172344418d6af30281a8
monowarhossain50000-coder <monowarhossain50000@gmail.com>
2026-08-22 23:11:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/api/status")
+def status():
+    return jsonify({
+        "status": "online",
+        "message": "AI Signal Pro is running"
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
