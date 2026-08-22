9a601ebf7f609f0b7cc58863f053125310f128e5
monowarhossain50000-coder <monowarhossain50000@gmail.com>
2026-08-22 21:40:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    signal = "⬆️ UP"
-    return signal
+    return "⬆️ UP"
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=5000)
